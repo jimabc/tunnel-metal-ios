773a13b0de392391390cdec3c1100c0282a3efba
Jim Matteson <jim@intuitiveuser.com>
2016-10-23 13:02:21 -0700

    Swift 3 support + texture updates
    Textures are rendering darker in Xcode 8, I lightened them so they
    don’t look terrible.

--- Tunnel.playground/Contents.swift
+++ Tunnel.playground/Contents.swift
@@ -1,8 +1,8 @@
 import MetalKit
-import XCPlayground
+import PlaygroundSupport
 
 let size = 512.0
 let device = MTLCreateSystemDefaultDevice()!
 let frame = CGRect(x:0, y:0, width:size, height:size)
 let view = MetalView(frame: frame, device: device)
-XCPlaygroundPage.currentPage.liveView = view
+PlaygroundPage.current.liveView = view
